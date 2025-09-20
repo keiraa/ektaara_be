8821d076c853e07c8982207e33bdea2d5b177c8e
sivamohannasina <sivamohannasina@gmail.com>
2025-09-20 18:29:47 +0530

    cart changes

--- src/main/java/com/ektaara/open_gem_gem/entity/Cart.java
+++ src/main/java/com/ektaara/open_gem_gem/entity/Cart.java
@@ -23,7 +23,7 @@ public class Cart {
     private Long id;
 
     @OneToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
     private User user;
 
     @JdbcTypeCode(SqlTypes.JSON)
